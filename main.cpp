23e3e1d9cff46134ebaf021c3458b3c89c677680
altoliaw <altoliaw@hotmail.com>
2021-05-21 22:51:01 +0800

    Finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stack>
 
 template <class CL1>
 class Node{
@@ -15,44 +14,42 @@ class Node{
 
 template<class CL1>
 class Tree{
-    public: std::stack<Node<CL1>*>* op_Sta2NoP;
     public: Node<CL1>* op_Root;
 
     public: Tree(CL1 cl1_Data){
         op_Root = nullptr;
-        op_Sta2NoP = new std::stack<Node<CL1>*>();
 
         op_Root = new Node<CL1>(cl1_Data);
-        op_Sta2NoP->push(op_Root);
-
     }
 
     public: ~Tree(){
-        for(;!op_Sta2NoP->empty();){
-            op_Sta2NoP->pop();
-        }
         fn_TraInOrder(op_Root, true);
     }
 
 
     public: void InsNode(CL1 cl1_Data){
-        Node<CL1>* op_Tmp = nullptr;
-        for(;
-            !op_Sta2NoP->empty() &&
-            cl1_Data > (op_Sta2NoP->top())->cl1_Field
-        ;){
-            op_Tmp = op_Sta2NoP->top();
-            op_Sta2NoP->pop();
-        }
-
         Node<CL1>* op_NewN = new Node<CL1>(cl1_Data);
-        if(op_Tmp != nullptr){
-            op_Tmp->op_Rt = op_NewN;
+
+        for(Node<CL1>* op_Tmp= op_Root;;){
+            if(op_Tmp->cl1_Field > cl1_Data){
+                if(op_Tmp->op_Lt == nullptr){
+                    op_Tmp->op_Lt = op_NewN;
+                    break;
+                }
+                else{
+                    op_Tmp = op_Tmp->op_Lt;
+                }
+            }
+            else{
+                if(op_Tmp->op_Rt == nullptr){
+                    op_Tmp->op_Rt = op_NewN;
+                    break;
+                }
+                else{
+                    op_Tmp = op_Tmp->op_Rt;
+                }
+            }
         }
-        else{
-            (op_Sta2NoP->top())->op_Lt = op_NewN;
-        }
-        op_Sta2NoP->push(op_NewN);
     }
 
     public: void fn_TraInOrder (Node<CL1>* op_Node, bool b_DMode =false){
